56a2577119a7b5fe3c78023eb0f5ffca6245cc88
Romildo Alves <raj.alves96@gmail.com>
2019-11-09 10:09:16 -0300

    Border checking and paddle and ball collisions

--- Pong.py
+++ Pong.py
@@ -32,6 +32,8 @@ ball.shape('square')
 ball.color('white')
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
 
 #Function
 def paddle_a_up():
@@ -64,3 +66,33 @@ wn.onkeypress(paddle_b_down, "Down")
 #Main game loop
 while True:
     wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290 :
+        ball.sety(290)
+        ball.dy *= -1 
+
+    if ball.ycor() < -290 :
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if  ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    if  ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    #Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
